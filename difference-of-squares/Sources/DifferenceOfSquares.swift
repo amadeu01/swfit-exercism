e40fb8c0ebc086d83c96994fe483d55e7ba4b67b
Amadeu Cavalcante <amadeu01@gmail.com>
2018-04-09 00:17:50 -0300

    Added difference of squares

---
+++ difference-of-squares/Sources/DifferenceOfSquares.swift
@@ -0,0 +1,21 @@
+import Foundation
+struct Squares {
+    let numberOfIntegers: Int
+    
+    var squareOfSums: Int {
+        return Int(pow(Double(((numberOfIntegers + 1) * numberOfIntegers)/2), 2.0))
+    }
+    
+    // From https://trans4mind.com/personal_development/mathematics/series/sumNaturalSquares.htm
+    var sumOfSquares: Int {
+        return Int(pow(Double(numberOfIntegers), 3.0)/3.0 + pow(Double(numberOfIntegers), 2.0)/2.0 + Double(numberOfIntegers)/6.0)
+    }
+    
+    var differenceOfSquares: Int {
+        return squareOfSums - sumOfSquares
+    }
+    
+    init(_ numberOfIntegers: Int) {
+        self.numberOfIntegers = numberOfIntegers
+    }
+}
