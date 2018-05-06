633c04f10bccd156e05cc94d558edb8cfca849e2
Amadeu Cavalcante <amadeu01@gmail.com>
2018-05-06 17:42:08 -0300

    Resolved Sum of Multiples

--- sum-of-multiples/Sources/SumOfMultiples.swift
+++ sum-of-multiples/Sources/SumOfMultiples.swift
@@ -1,5 +1,33 @@
 import Foundation
 
 class SumOfMultiples {
+    static func findMutiples(_ limit: Int, for multiple: Int) -> [Int] {
+        if multiple >= limit {
+            return [Int]()
+        }
+        let numbers = multiple..<limit
+        return numbers.filter {
+            if multiple == 0 {
+                return false
+            }
+            
+            if $0 == 0 {
+                return false
+            }
+            
+            if $0 > multiple {
+                return $0 % multiple == 0
+            } else {
+                return multiple % $0 == 0
+            }
+            
+        }
+    }
     
+    static func toLimit(_ limit: Int, inMultiples: [Int]) -> Int {
+        let arrayOfMultiples = inMultiples.map { findMutiples(limit, for: $0) }
+        let multiples = Array(Set(arrayOfMultiples.joined()))
+        let response = multiples.reduce(0, +)
+        return response
+    }
 }
